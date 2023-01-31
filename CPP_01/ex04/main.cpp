a244fbea743b8b48d3b4236140687f8429224585
Steven Mayrand <smayrand@c2r1p9.42quebec.com>
2023-01-31 16:22:57 -0500

    ex04 done

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smayrand <smayrand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 14:58:42 by smayrand          #+#    #+#             */
-/*   Updated: 2023/01/31 16:13:36 by smayrand         ###   ########.fr       */
+/*   Updated: 2023/01/31 16:20:06 by smayrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,25 @@ int main(int argc, char **argv)
 {
 	std::string		tmp;
 	size_t			index;
-	size_t			s1len;
-	size_t			s2len;
-	
+
 	if(argc != 4)
-		std::cout << "Need 4 arguments" << std::endl;
+		std::cout << "Require 4 arguments" << std::endl;
 	else
 	{
 		std::ifstream	in(argv[1]);
 		std::string		s1(argv[2]);
 		std::string		s2(argv[3]);
 		std::ofstream	out("resultat");
-		
-		s1len = s1.length();
-		s2len = s2.length();
 		while(getline(in, tmp, '\0'))
 		{
-			// index != std::string::npos = fin du fichier. équivalent c++ de while(tmp[index] != '\0')	
-			while(index != std::string::npos)
+			while(index != std::string::npos) // = while not at eof
 			{
 				if(tmp.find(s1))
 				{
 					index = tmp.find(s1);
-					tmp.erase(index, s1len);
+					tmp.erase(index, s1.length());
 					tmp.insert(index, s2);	
-					index = tmp.find(s1, index + s2len);
+					index = tmp.find(s1, index + s2.length());
 				}
 			}
 			out << tmp;	
