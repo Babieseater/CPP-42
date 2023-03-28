a4f934c79fa6f244eaedc21f33e70e2dd2ebf17a
Steven Mayrand <smayrand@c2r6p1.42quebec.com>
2023-03-28 15:19:41 -0400

    Backup

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smayrand <smayrand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 15:10:54 by smayrand          #+#    #+#             */
-/*   Updated: 2023/01/30 11:10:03 by smayrand         ###   ########.fr       */
+/*   Updated: 2023/03/28 15:15:50 by smayrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,14 @@ int main(void)
 	
 	std::cout << "\033[32m============= ADDRESS =============\033[0m" << std::endl;
 	
-	std::cout << "string address : " << &brain << std::endl;
-	std::cout << "PTR address : " << &stringPTR << std::endl;
-	std::cout << "REF address : " << &stringREF << std::endl;
+	std::cout << " STR address : " << &brain << std::endl;
+	std::cout << "&PTR address : " << &stringPTR << std::endl;
+	std::cout << " PTR address : " << stringPTR << std::endl;
+	std::cout << " REF address : " << &stringREF << std::endl;
 
 	std::cout << "\033[32m============== VALUE ==============\033[0m" << std::endl;
 	
-	std::cout << "string value : " << brain << std::endl;
+	std::cout << "STR value : " << brain << std::endl;
 	std::cout << "PTR value : " << *stringPTR << std::endl;
 	std::cout << "REF value : " << stringREF << std::endl;
 	
