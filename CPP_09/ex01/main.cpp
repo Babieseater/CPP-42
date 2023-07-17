d6a0306baa9d5849e867e06682776f66ee01f16a
Steven Mayrand <smayrand@c2r6p6.42quebec.com>
2023-07-17 14:15:04 -0400

    BIGGESTIFCHALLENGE

--- CPP_09/ex01/main.cpp
+++ CPP_09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smayrand <smayrand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 11:56:02 by smayrand          #+#    #+#             */
-/*   Updated: 2023/07/04 19:40:30 by smayrand         ###   ########.fr       */
+/*   Updated: 2023/07/17 13:55:54 by smayrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,29 @@ int	main(int argc, char **argv)
 {
 	std::stack<int> rpn;
 	int res = 0;
+	int tmp = 0;
 	int i = 0;
 
 	if(argc == 2 && isdigit(argv[1][0]) != 0)
 	{
-		while(argv[1][i] != '\0')
+		while(argv[1][i] )
 		{
-			if ((i > 5 && isdigit(argv[1][i]) != 0 && isdigit(argv[1][i + 2]) != 0) 
-				|| isdigit(argv[1][i]) != 0 && argv[1][i + 1] != ' '
-					|| (strchr("+-*/", argv[1][i]) != 0 && argv[1][i + 1] != ' ' && argv[1][i + 1] != '\0') 
-						|| (argv[1][i] == ' ' && argv[1][i + 1] == '\0')
-							|| (strchr("+-/*", argv[1][i]) != 0 && strchr("+-/*", argv[1][i + 2]) != 0))
+			if (isdigit(argv[1][i]) != 0 && argv[1][i + 1] != ' ' || (strchr("+-*/", argv[1][i]) != 0 && argv[1][i + 1] != ' ' && argv[1][i + 1] != '\0') 
+						|| (argv[1][i] == ' ' && argv[1][i + 1] == '\0') || (argv[1][i] == ' ' && argv[1][i + 1] == ' ') || isalpha(argv[1][i]) != 0
+							|| (isdigit(argv[1][i - 2]) == 0 && isdigit(argv[1][i]) != 0 && strchr("+-*/",argv[1][i + 2]) != 0 && strchr("+-*/",argv[1][i + 4]) != 0 && i > 6)
+								||  (isdigit(argv[1][i]) != 0 &&  isdigit(argv[1][i + 2]) != 0 && isdigit(argv[1][i + 4]) != 0)								
+									|| (strchr("0", argv[1][i]) != 0 && strchr("/", argv[1][i + 2]) != 0))
 				return (std::cout << "Error: Bad Format." << std::endl, 0);
 			if(isdigit(argv[1][i]) != 0 && argv[1][i + 1] != '\0')
 			{
 				rpn.push(argv[1][i] - '0');
-				if(isdigit(argv[1][i + 2]) == 1)
+				if(!res && isdigit(argv[1][i + 2]) == 1 && tmp == 0)
 					res = rpn.top() - 0;
+				else if(res && isdigit(argv[1][i + 2]) == 1)
+				{
+					tmp = res - 0;
+					res = rpn.top() - 0;
+				}
 			}
 			else if(argv[1][i] == '+' || argv[1][i] == '-' || argv[1][i] == '*' || argv[1][i] == '/' )
 			{
@@ -43,9 +49,32 @@ int	main(int argc, char **argv)
 				else if(argv[1][i] == '*')
 				res = res * rpn.top();
 				else if(argv[1][i] == '/')
-				res = res / rpn.top();
+				{
+					if (rpn.top() == '0')
+						return (std::cout << "Error: Bad Format." << std::endl, 0);
+					res = res / rpn.top();
+				}
 				rpn.pop();
 				rpn.push(res - '0');
+			}
+			if (strchr("+-*/", argv[1][i]) != 0 && strchr("+-*/", argv[1][i + 2]) != 0 && tmp)
+			{
+				if(argv[1][i + 2] == '+')
+				res = res + tmp;
+				else if(argv[1][i + 2] == '-')
+				res = res - tmp;
+				else if(argv[1][i + 2] == '*')
+				res = res * tmp;
+				else if(argv[1][i + 2] == '/')
+				{
+					if (tmp == '0')
+						return (std::cout << "Error: Bad Format." << std::endl, 0);
+					res = res / rpn.top();
+				}
+				rpn.pop();
+				rpn.push(res - '0');
+				tmp = 0;
+				i += 2;
 			}
 			i++;
 		}
